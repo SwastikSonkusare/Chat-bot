08eb552d3d6c180cb52c5e749348d3c80774db9c
Swastik <swastik.sonkusare18@gmail.com>
2023-05-29 12:40:21 +0530

    made some changes

--- src/MessageParser.js
+++ src/MessageParser.js
@@ -15,7 +15,6 @@ const MessageParser = ({ children, actions }) => {
   };
 
   const parse = (message) => {
-    console.log(message);
     const lowercase = message.toLowerCase();
 
     if (lowercase.includes("got it")) {
